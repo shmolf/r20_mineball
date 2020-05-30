141a4395ac1ca57ba4ea013cb72e7428e52dbcf1
Nicholas Browning <shmolf@gmail.com>
2020-05-30 17:08:35 -0400

    ESLint & WebPack Setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+      app: './src/MineBall.js',
+  },
+  mode: 'production',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      App: path.resolve(__dirname, 'src/'),
+      Module: path.resolve(__dirname, 'src/modules/')
+    }
+  }
+};
