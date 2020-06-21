4aa436ec74bf6dfd495aff3c859a859890be6342
Nicholas Browning <shmolf@gmail.com>
2020-06-21 02:10:52 -0400

    Early Action organizing

--- src/Cards/Actions.js
+++ src/Cards/Actions.js
@@ -1,13 +1,122 @@
 
+/* ----- Attack Start ----- */
+
 /**
  * @param {Object} sourceToken - Token initiating the attack
  * @param {Object} targetToken - The defending token
  * @returns {boolean}
  */
-function attack(sourceToken, targetToken) {
+function Enchant(sourceToken, targetToken) {
   return { sourceToken, targetToken };
 }
 
+/**
+ * @param {Object} sourceToken - Token initiating the attack
+ * @param {Object} targetToken - The defending token
+ * @returns {boolean}
+ */
+function Launch(sourceToken, targetToken) {
+  return { sourceToken, targetToken };
+}
+
+/**
+ * @param {Object} sourceToken - Token initiating the attack
+ * @param {Object} targetToken - The defending token
+ * @returns {boolean}
+ */
+function Shoot(sourceToken, targetToken) {
+  return { sourceToken, targetToken };
+}
+
+/**
+ * @param {Object} sourceToken - Token initiating the attack
+ * @param {Object} targetToken - The defending token
+ * @returns {boolean}
+ */
+function Simple(sourceToken, targetToken) {
+  return { sourceToken, targetToken };
+}
+
+/* ----- Attack End ----- */
+
+/* ----- Movement Start ----- */
+
+/**
+ * @param {Object} sourceToken - Token initiating the attack
+ * @param {Object} targetToken - The defending token
+ * @returns {boolean}
+ */
+function Fly(sourceToken, targetToken) {
+  return { sourceToken, targetToken };
+}
+
+/**
+ * @param {Object} sourceToken - Token initiating the attack
+ * @param {Object} targetToken - The defending token
+ * @returns {boolean}
+ */
+function Jump(sourceToken, targetToken) {
+  return { sourceToken, targetToken };
+}
+
+/**
+ * @param {Object} sourceToken - Token initiating the attack
+ * @param {Object} targetToken - The defending token
+ * @returns {boolean}
+ */
+function Swap(sourceToken, targetToken) {
+  return { sourceToken, targetToken };
+}
+
+/* ----- Movement End ----- */
+
+/* ----- Utility Start ----- */
+
+/**
+ * @param {Object} sourceToken - Token initiating the attack
+ * @param {Object} targetToken - The defending token
+ * @returns {boolean}
+ */
+function Repair(sourceToken, targetToken) {
+  return { sourceToken, targetToken };
+}
+
+/**
+ * @param {Object} sourceToken - Token initiating the attack
+ * @param {Object} targetToken - The defending token
+ * @returns {boolean}
+ */
+function Vitiate(sourceToken, targetToken) {
+  return { sourceToken, targetToken };
+}
+
+/**
+ * @param {Object} sourceToken - Token initiating the attack
+ * @param {Object} targetToken - The defending token
+ * @returns {boolean}
+ */
+function Hide(sourceToken, targetToken) {
+  return { sourceToken, targetToken };
+}
+
+/* ----- Utility End ----- */
+
+
 export default {
-  attack,
+  attack: {
+    Enchant,
+    Launch,
+    Shoot,
+    Simple,
+  },
+  movement: {
+    Fly,
+    Jump,
+    Swap,
+  },
+  utility: {
+    Repair,
+    Vitiate,
+    Hide,
+  },
 };
---
+++ src/Cards/Events.js
@@ -0,0 +1,47 @@
+
+export default {
+  block: {
+    'Fly Over': () => {},
+    'Jump Over': () => {},
+    Named: () => {},
+    Vitiate: () => {},
+  },
+  force: {
+    'Card On Opponent': () => {},
+    'Card swap': () => {},
+    'Opponent Discard': () => {},
+    Rotate: () => {},
+    'Show Opponent Card(s)': () => {},
+    'Swap Card': () => {},
+  },
+};
+/*
+Bonus/Weakness, Element
+Bonus/Weakness, Job
+Bonus/Weakness, Named
+Bonus/Weakness, Terrain Proximity
+Bonus/Weakness, Title
+Bonus/Weakness, With Attribute
+Cardinal, Block Action
+Clock Tick, Bank
+Clock Tick, Steal
+Convert Terrain
+Hidden, Craft With Attribute
+Immediate Play, Else Perform Action
+Immunty/Loses To, Element
+Immunty/Loses To, Job
+Immunty/Loses To, Named
+Immunty/Loses To, Title
+Immunty/Loses To, With Attribute
+On Deploy, Action
+Ordinal, Block Action
+Remove, Card From Board
+Sacarifice Card Plus Another, Perform Action
+Sacarifice Card, Perform Action
+Set, General
+Tie, Defeat
+Tie, Victorious
+Turn, Gain
+Turn, Lose
+Victorious/Defeated, Perform Action
+*/
