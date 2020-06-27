1e0a363a39119bc11a6f60fcbc3588b17d0e57e8
BigFatMikeL <mike@lakner.cc>
2020-06-27 12:05:59 -0400

    Try 2

--- src/Commands/StartCommand.js
+++ src/Commands/StartCommand.js
@@ -1,4 +1,46 @@
-// -----
-// This module was added by Mike Lakner to house the code supporting the Start command and sub commands
-// but he is clueless and needs a bigger brain.
-// -----
+/**
+ * @namespace App.Commands
+ */
+
+import Command from 'Commands/Command';
+import { mbStartNewGame } from 'App/modules/Game';
+
+export default class StartCommand extends Command {
+  constructor() {
+    super();
+
+    this.cmd = 'start';
+    this.desc = 'Start command for various game actions such as NewGame, SavedGame etc.';
+    this.func = this.runSubCommand;
+    /**
+     * @param {string} who - layer's human name
+     * @param {string} playerId - reference of the player
+     * @param {string[]} args - arguments for the command
+     */
+    // eslint-disable-next-line no-unused-vars
+    this.help = (who, playerId, args) => {
+      sendChat(
+        'Mine Ball Help',
+        `/w ${who}
+        <p>Start NewGame - Clear the board and start a new game.</p>
+        <p>Start SavedGame - Start a previously saved game.</p>
+        `,
+      );
+    };
+
+    this.subCommands = {
+      newgame: {
+        gmOnly: false,
+        internal: false,
+        func: mbStartNewGame,
+        paramList: [],
+      },
+      savedGame: {
+        gmOnly: false,
+        internal: false,
+        func: log('*** SavedGame still needs to be implimented.'),
+        paramList: [],
+      },
+    };
+  }
+}
