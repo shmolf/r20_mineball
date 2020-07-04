bab5b2fcb12d4b3bea7b1982428608847bbfc058
BigFatMikeL <mike@lakner.cc>
2020-07-04 17:12:13 -0400

    Added Show command

---
+++ src/Commands/ShowCommand.js
@@ -0,0 +1,46 @@
+/**
+ * @namespace App.Commands
+ */
+
+import Command from 'Commands/Command';
+import { mbShowGameState } from 'App/modules/State';
+
+export default class ShowCommand extends Command {
+  constructor() {
+    super();
+
+    this.cmd = 'show';
+    this.desc = 'Show command for various game data such as state.';
+    this.func = this.runSubCommand;
+    /**
+     * @param {string} who - layer's human name
+     * @param {string} playerId - reference of the player
+     * @param {string[]} args - arguments for the command
+     */
+    // eslint-disable-next-line no-unused-vars
+    this.help = (who, playerId, args) => {
+      sendChat(
+        'Mine Ball Help',
+        `/w ${who}
+        <p>Show GameState - Dump the game state to the chat window.</p>
+        <p>Show SavedGames - Show all of the previously saved games.</p>
+        `,
+      );
+    };
+
+    this.subCommands = {
+      newgame: {
+        gmOnly: false,
+        internal: false,
+        func: mbShowGameState,
+        paramList: [],
+      },
+      savedGame: {
+        gmOnly: false,
+        internal: false,
+        func: log('*** SavedGames still needs to be implimented.'),
+        paramList: [],
+      },
+    };
+  }
+}
